cbdbd01aa40fbe7ae46f91a1ae7daf07b708624b
SnKDaisuke <66209329+SnKDaisuke@users.noreply.github.com>
2021-05-30 16:14:58 +0200

    commit 30/05
    +ADD image for README

--- app/src/main/java/com/example/nemesis/presentation/list/Choice.kt
+++ app/src/main/java/com/example/nemesis/presentation/list/Choice.kt
@@ -9,8 +9,8 @@ data class Choice(
 ) {
 
     companion object {
-        val choice1 = Choice(1, "NextSeason", "You'll see the list of future airing anime next season","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBdUeR62GBEMSRHxatJ7oY-MV3G-Yv-MK6cg&usqp=CAU")
-        val choice2 = Choice(2, "Upcomming", "You'll see the list of announcing anime release", "https://wallpapercave.com/wp/wp5591884.png")
+        val choice1 = Choice(1, "Airing Now", "You'll see the list of airing anime","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBdUeR62GBEMSRHxatJ7oY-MV3G-Yv-MK6cg&usqp=CAU")
+        val choice2 = Choice(2, "Upcoming", "You'll see the list of announcing anime release", "https://wallpapercave.com/wp/wp5591884.png")
 
     }
 }
